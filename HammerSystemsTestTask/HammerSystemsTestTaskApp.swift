e2da14dfed9e0c13763eec056431fa14e9daf1d3
Artem Legostinov <legostinov.artyom@gmail.com>
2025-07-23 17:29:03 +0300

    Initial Commit

---
+++ HammerSystemsTestTask/HammerSystemsTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HammerSystemsTestTaskApp.swift
+//  HammerSystemsTestTask
+//
+//  Created by Артём on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HammerSystemsTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
